0712a055e1e434b1a882e2a191eda631cc32cc79
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-11-10 15:20:51 -0500

    initial commit. Starting sprint now

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [players, setPlayers] = useState([]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
